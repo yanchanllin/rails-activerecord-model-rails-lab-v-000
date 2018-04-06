1f4a3313f486dadd985cf765653c4cc95783b03a
YanC Lin <yonalin1@gmail.com>
2018-04-06 03:11:48 +0000

    automatically backed up by learn

--- db/migrate/20180406031101_create_students.rb
+++ db/migrate/20180406031101_create_students.rb
@@ -1,7 +1,8 @@
 class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
-      t.string :title
+      t.string :first_name
+      t.string :last_name
     end
   end
 end
